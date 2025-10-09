30f34eb36e22156855f53b04101e24246a0fc934
NexusOnePlus <wv35788@gmail.com>
2025-10-08 21:50:31 -0500

    feat: update and version for system tray

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,12 +3,64 @@ using System.Drawing;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Forms;
+using Velopack;
+using MessageBox = System.Windows.MessageBox;
+using Velopack.Sources;
+using System.Windows.Controls;
 
 namespace Cake;
 
 public partial class App : System.Windows.Application
 {
     private NotifyIcon? _trayIcon;
+
+    private String _version= "1.0.0";
+    [STAThread]
+    private static void Main(string[] args)
+    {
+
+        bool createdNew;
+        using (var mutex = new System.Threading.Mutex(true, "IconizerAppMutex", out createdNew))
+        {
+            if (!createdNew)
+            {
+                MessageBox.Show("App already running.", "Unique instance", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            try
+            {
+                VelopackApp.Build().Run();
+
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fatal error on startup runtime: {ex.Message}", "Start failed", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+
+    public void ActualVersion()
+    {
+        var token = "";
+        var source = new Velopack.Sources.GithubSource("https://github.com/NexusOnePlus/Cake", token, true);
+        var updateManager = new UpdateManager(source);
+        string version;
+        if (updateManager.IsInstalled)
+        {
+            version = updateManager.CurrentVersion?.ToString() ?? "Unknown";
+        }
+        else
+        {
+            version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Debug";
+        }
+        _version = version;
+    }
+
+
 
     protected override void OnStartup(StartupEventArgs e)
     {
@@ -28,7 +80,14 @@ public partial class App : System.Windows.Application
         };
 
         var contextMenu = new ContextMenuStrip();
-        contextMenu.Items.Add("Salir", null, (s, ev) => Shutdown());
+        ActualVersion();
+        contextMenu.Items.Add("Version: " + _version);
+        contextMenu.Items.Add("Update", null, (s, ev) => {
+            Update();
+            ActualVersion();
+            }
+        ); 
+        contextMenu.Items.Add("Exit", null, (s, ev) => Shutdown());
         _trayIcon.ContextMenuStrip = contextMenu;
     }
 
@@ -45,4 +104,62 @@ public partial class App : System.Windows.Application
 
         base.OnExit(e);
     }
+
+
+
+    private async void Update()
+    {
+        var token = "";
+        var source = new Velopack.Sources.GithubSource("https://github.com/NexusOnePlus/Cake", token, true);
+        var updateManager = new UpdateManager(source);
+        try
+        {
+            if (!updateManager.IsInstalled)
+            {
+                MessageBox.Show("Updates can only be checked in an installed version of the application.", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            try
+            {
+                var newVersion = await updateManager.CheckForUpdatesAsync();
+                if (newVersion == null)
+                {
+                    MessageBox.Show("Your application is up to date.", "No Updates", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+
+                try
+                {
+                    await updateManager.DownloadUpdatesAsync(newVersion);
+                }
+                catch (Exception exDownload)
+                {
+                    MessageBox.Show($"Error during download:\n{exDownload}", "Download Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                try
+                {
+                    updateManager.ApplyUpdatesAndRestart(newVersion);
+                }
+                catch (Exception exApply)
+                {
+                    MessageBox.Show($"Error applying update:\n{exApply}", "Apply Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+            catch (Exception exCheck)
+            {
+                MessageBox.Show($"Error checking for updates:\n{exCheck}", "Check Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"General error in update process:\n{ex}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+
 }
